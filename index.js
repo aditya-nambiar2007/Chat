00a14559c5a357b497a1f945296770d5d3f9dea9
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2023-04-09 16:24:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-let ws = require('ws')
-let fs = require('fs')
-let url = require('url')
-const { error } = require('console')
-const server = require('http').createServer(function (req, res) {
-    fs.readFile('xx.html', function (err, data) {
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+let ws= require('ws')
+const server = http.createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = "." + q.pathname;
+    if (filename.endsWith("/") || filename=="./index.js"||/\.\/Room\/./.test(q.pathname)){
+        filename="./index.html"
+    }
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
@@ -12,17 +21,24 @@ const server = require('http').createServer(function (req, res) {
 
 
 
-
 const wss = new ws.WebSocketServer({ server })
-fs.appendFile('data.txt', "", (e) => { if (e) console.log(e) })
 wss.on("connection", ws => {
-    fs.readFile("data.txt", (err, data) => {
-        if (err) console.error(error)
-        ws.send(data)
-    })
     ws.on('message', m => {
-        require('fs').appendFile('data.txt', m, (e) => { if (e) console.log(e) })
+        let x = JSON.parse(m)
+        if(x[1]=='load'){
+            try{
+            fs.appendFile("Rooms/"+x[0]+".txt", "", (e) => { if (e) console.log(e) })
+            fs.readFile("Rooms/"+x[0]+".txt", function (err, data) {
+                if (err) {console.error(err)}
+                ws.send( JSON.stringify( [x[0],data] ) )
+            })}
+            catch (error){
+                ws.send( JSON.stringify( [x[0],''] ) )
+            }
+        }
+        else{
+        require('fs').appendFile(`Rooms/${x[0]}.txt`, x[1], (e) => { if (e) console.log(e) })
         wss.clients.forEach(e => { e.send(m) });
-    })
+    }})
 })
 server.listen(80, () => { console.log('started'); })
